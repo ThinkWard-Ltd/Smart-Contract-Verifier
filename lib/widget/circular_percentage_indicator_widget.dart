608f5e99e25d44dd60a72a8dde061e51abf7a297
Kevin <kevindupreez8@gmail.com>
2021-05-31 05:01:30 +0200

    Circular Percentage indicators for dashboard

---
+++ lib/widget/circular_percentage_indicator_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+class CircularPercentageIndicator extends StatelessWidget {
+
+  final int percentage;
+
+  CircularPercentageIndicator(this.percentage);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(10),
+        child: CircularPercentIndicator(
+          radius: 120.0,
+          lineWidth: 10.0,
+          animation: true,
+          percent: percentage / 100,
+          center: Text(
+            percentage.toString() + "%",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w600,
+              color: Colors.teal,),
+          ),
+          backgroundColor: Color.fromRGBO(105, 105, 105, 1),
+          circularStrokeCap: CircularStrokeCap.round,
+          progressColor: Colors.tealAccent,
+        )
+    );
+  }
+}
--- lib/widget/dashboard_widget.dart
+++ lib/widget/dashboard_widget.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widget/circular_percentage_indicator_widget.dart';
 
 class Dashboard extends StatelessWidget {
   final dashDataList = [
-    {'name': 'Contract A','percentage':10},
-    {'name': 'Contract B','percentage':40},
-    {'name': 'Contract C','percentage':60},
+    {'name': 'Contract A', 'percentage': 10},
+    {'name': 'Contract B', 'percentage': 40},
+    {'name': 'Contract C', 'percentage': 60},
   ];
 
   @override
@@ -12,20 +13,14 @@ class Dashboard extends StatelessWidget {
     var name;
     var percentage;
     return GridView.count(
-      crossAxisCount: 1,
-      children: List.generate(
-        dashDataList.length,
-        (index) {
-          name = dashDataList[index]['name'];
-          percentage = dashDataList[index]['percentage'];
-          return Center(
-            child: Text(
-              "$name $percentage",
-              style: Theme.of(context).textTheme.headline5,
-            ),
-          );
-        },
-      ),
-    );
+        crossAxisCount: 1,
+        children: List.generate(
+          dashDataList.length,
+          (index) {
+            name = dashDataList[index]['name'];
+            percentage = dashDataList[index]['percentage'];
+            return CircularPercentageIndicator(percentage);
+          },
+        ));
   }
 }
